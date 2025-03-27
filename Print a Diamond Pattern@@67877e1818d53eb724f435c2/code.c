5d9f3b41a9d4b5bad9333f15876096f7c233fb99
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-27 15:06:51 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,25 +1,54 @@
-#include<stdio.h>
-int main(){
+// #include<stdio.h>
+// int main(){
+//     int N;
+//     scanf("%d",&N);
+//     int ml=N/2+1;
+//     int nst=1;
+//     int nsp=N/2;
+//     for(int i=1;i<=2*N-1;i++){
+//         for(int j=1;j<=nsp;j++){
+//             printf(" ");
+//         }
+//         for(int j=1;j<=nst;j++){
+//             printf("*");
+//         }
+//         if(i<ml){
+//             nst+=2;
+//             nsp--;
+//         }
+//         else{
+//             nst-=2;
+//             nsp++;
+//         }
+//         printf("\n");
+//     }
+// }
+#include <stdio.h>
+
+int main() {
     int N;
-    scanf("%d",&N);
-    int ml=N/2+1;
-    int nst=1;
-    int nsp=N/2;
-    for(int i=1;i<=2*N-1;i++){
-        for(int j=1;j<=nsp;j++){
+    scanf("%d", &N);
+    int ml = (N + 1) / 2; // Calculate mid line
+    int nst = 1; // Number of stars
+    int nsp = N / 2; // Number of spaces
+
+    for (int i = 1; i <= 2 * N - 1; i++) {
+        // Print spaces
+        for (int j = 1; j <= nsp; j++) {
             printf(" ");
         }
-        for(int j=1;j<=nst;j++){
+        // Print stars
+        for (int j = 1; j <= nst; j++) {
             printf("*");
         }
-        if(i<ml){
-            nst+=2;
+        if (i < ml) {
+            nst += 2;
             nsp--;
-        }
-        else{
-            nst-=2;
+        } else {
+            nst -= 2;
             nsp++;
         }
         printf("\n");
     }
+    return 0;
 }
