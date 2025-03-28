4334f57ee6e2bec6576d8f708ffe56cbd0bebe9e
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-28 17:07:51 +0530

    Solution to Find the Maximum and Minimum Element in an Array

--- Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -1,68 +1,36 @@
-// Your code here...
 
-#include<stdio.h>
-#include<limits.h>
-int main(){
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n];
-    scanf("%d ",&arr[i]);
-    int max=INT_MIN;
-    int min=INT_MIN;
-    for(int i=0;i<n;i++){
-        if(arr[i]<max){
-            arr[i]=max;
-            
-        }
+    scanf("%d", &n);  // Read the size of the array
 
-    }
-     for(int i=0;i<n;i++){
-        if(arr[i]>min){
-            arr[i]=min;
-        
-        }
-        printf("%d ",min);
-        printf("%d ",max);
+    int arr[n];  // Declare an array of size n
+
+    // Read the array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
+    int max = INT_MIN; // Initialize max as smallest possible integer
+    int min = INT_MAX; // Initialize min as largest possible integer
 
+    // Finding the maximum value
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
 
+    // Finding the minimum value
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    printf("%d ", min);
+    printf("%d ", max);
+
+    return 0;
 }
-
-
-
-// #include <stdio.h>
-// #include <limits.h>
-
-// int main() {
-//     int n;
-//     scanf("%d", &n);  // Read the size of the array
-
-//     int arr[n];  // Declare an array of size n
-
-//     // Read the array elements
-//     for (int i = 0; i < n; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-
-//     int max = INT_MIN; // Initialize max as smallest possible integer
-//     int min = INT_MAX; // Initialize min as largest possible integer
-
-//     // Finding the maximum value
-//     for (int i = 0; i < n; i++) {
-//         if (arr[i] > max) {
-//             max = arr[i];
-//         }
-//     }
-
-//     // Finding the minimum value
-//     for (int i = 0; i < n; i++) {
-//         if (arr[i] < min) {
-//             min = arr[i];
-//         }
-//     }
-//     printf("%d ", min);
-//     printf("%d ", max);
-
-//     return 0;
-// }
