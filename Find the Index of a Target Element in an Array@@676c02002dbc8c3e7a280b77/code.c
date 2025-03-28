1cd92cbb094aeec753c730dfcbcc95ad9fc0f497
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 02:44:15 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,24 +1,50 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-    int N;
-    int target;
-    scanf("%d %d",&N,&target);
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int N;
+//     int target;
+//     scanf("%d %d",&N,&target);
+//     int arr[N];
+//     for(int i=0;i<N;i++){
+//         scanf("%d ",&arr[i]);
+//     }
+//     int check=0;
+//     for(int i=0;i<N;i++){
+//         if(arr[i]==target){
+//             printf("%d",i);
+//             break;
+//         }
+//         else{
+//             printf("%d",-1);
+//             break;
+//         }
+//     }
+
+// }
+#include <stdio.h>
+
+int main() {
+    int N, target;
+    scanf("%d %d", &N, &target); // Read N and target value
+
     int arr[N];
-    for(int i=0;i<N;i++){
-        scanf("%d ",&arr[i]);
-    }
-    int check=0;
-    for(int i=0;i<N;i++){
-        if(arr[i]==target){
-            printf("%d",i);
-            int check=1;
-            break;
-        }
-       
-    }
-    if(check==0){
-        printf("%d",-1);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]); // Read array elements correctly
     }
 
+    int found = 0; // Flag to check if target is found
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == target) {
+            printf("%d\n", i); // Print index of first occurrence
+            found = 1; // Mark that target is found
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("-1\n"); // Print -1 if target is not found
+    }
+
+    return 0;
 }
