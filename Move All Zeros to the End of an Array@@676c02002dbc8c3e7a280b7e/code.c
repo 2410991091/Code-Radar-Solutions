e0e450000a007c52914265421dc2dbd405adbda3
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 10:59:53 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,31 +1,64 @@
-// Your code here...
-#include<stdio.h>
-void swapzero(int arr[],int N){
-    int i=0;
-    int j=N-1;
-      for(int i=0;i<N;i++){
-        if(arr[i]==0){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            j--;
+// // Your code here...
+// #include<stdio.h>
+// void swapzero(int arr[],int N){
+//     int i=0;
+//     int j=N-1;
+//       for(int i=0;i<N;i++){
+//         if(arr[i]==0){
+//             int temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//             j--;
 
+//         }
+//         return;
+//     }
+// }
+// int main(){
+//     int N;
+//     scanf("%d",&N);
+//     int arr[N];
+//     for(int i=0;i<N;i++){
+//         scanf("%d ",&arr[i]);
+//     }
+//     swapzero(arr,N);
+//      for(int i=0;i<N;i++){
+//         printf("%d ",arr[i]);
+//     }
+// }
+
+#include <stdio.h>
+
+void moveZerosToEnd(int arr[], int N) {
+    int j = 0; // Position for the next nonzero element
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] != 0) {
+            // Swap nonzero element with arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
         }
-        return;
-    }
-}
-int main(){
-    int N;
-    scanf("%d",&N);
-    int arr[N];
-    for(int i=0;i<N;i++){
-        scanf("%d ",&arr[i]);
-    }
-    swapzero(arr,N);
-     for(int i=0;i<N;i++){
-        printf("%d ",arr[i]);
     }
 }
 
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    moveZerosToEnd(arr, N);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
 
 
