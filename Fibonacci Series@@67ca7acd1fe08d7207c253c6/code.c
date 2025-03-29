3c681633c7b97b57907b204677b2415b9cb38500
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 21:25:20 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+void fibonacciSeries(int n){
+    int fibonacci(int n) {
+    if (n <= 1)  // Base cases
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);  // Recursive call
+}
+}
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n<=0){
+        printf("Invalid input\n");
+        return 0;
+    }
+    fibonacciSeries(n);
+}
