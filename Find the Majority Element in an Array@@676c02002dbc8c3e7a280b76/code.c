a56b974e1337fb1f5d3dca12f9e2ec6154a09f6c
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 11:33:24 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,24 +1,56 @@
 // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int N;
+//     scanf("%d",&N);
+//     int arr[N];
+//     for(int i=0;i<N;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int a=-1;
+//     int majority=0;
+//     int check=0;
+//     for(int i=0;i<N;i++){
+//         if(arr[i]==arr[i+1]){
+//             majority++;
+//             check=1;
+//             break;
+//         }
+//     }
+//     if(check==1)printf("%d",arr[i]);
+//     else{
+//         printf("%d",a);
+//     }
+// }
+
+
 #include<stdio.h>
-int main(){
+
+int main() {
     int N;
-    scanf("%d",&N);
+    scanf("%d", &N);
+
     int arr[N];
-    for(int i=0;i<N;i++){
-        scanf("%d",&arr[i]);
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
     }
-    int a=-1;
-    int majority=0;
-    int check=0;
-    for(int i=0;i<N;i++){
-        if(arr[i]==arr[i+1]){
-            majority++;
-            check=1;
-            break;
+
+    int a = -1; // Default value if no majority element is found
+    int majority = -1; // Store majority element
+    int check = 0;
+
+    for(int i = 0; i < N-1; i++) { // Run only till N-2 to avoid out-of-bounds
+        if(arr[i] == arr[i+1]) {  // Corrected comparison operator
+            majority = arr[i];
+            check = 1;
+            break; // Stop as soon as a majority is found
         }
     }
-    if(check==1)printf("%d",arr[i]);
-    else{
-        printf("%d",a);
-    }
+
+    if(check == 1)
+        printf("%d", majority);  // Print the found majority element
+    else
+        printf("%d", a);  // Print -1 if no majority is found
+
+    return 0;
 }
