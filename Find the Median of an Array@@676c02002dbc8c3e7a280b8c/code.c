70eedebde6c67a718920135f52a95b6a9a5a593d
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 14:32:44 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,24 +1,63 @@
 // Your code here...
-#include<stdio.h>
+// #include<stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d", &n);  // Read the size of the array
+
+//     int arr[n];  // Declare an array of size n
+
+//     // Read the array elements
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+//     int median;
+//       for (int i = 0; i < n; i++) {
+//     if(i%2!=0){
+//         median=arr[n/2+1];
+       
+//     }
+//     else{
+//         median=arr([n/2]+[n/2+1]);
+//     }
+//     printf("%d",median);
+//     }
+// }
+
+
+
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     int n;
-    scanf("%d", &n);  // Read the size of the array
+    scanf("%d", &n); // Read the size of the array
 
-    int arr[n];  // Declare an array of size n
+    int arr[n]; // Declare an array of size n
 
     // Read the array elements
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int median;
-      for (int i = 0; i < n; i++) {
-    if(i%2!=0){
-        median=arr[n/2+1];
-       
+
+    // Sort the array
+    sortArray(arr, n);
+
+    double median;
+    if (n % 2 != 0) {
+        median = arr[n / 2];
+    } else {
+        median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
     }
-    else{
-        median=arr([n/2]+[n/2+1]);
-    }
-    printf("%d",median);
-    }
+
+    printf("Median: %.2f\n", median);
+    return 0;
 }
