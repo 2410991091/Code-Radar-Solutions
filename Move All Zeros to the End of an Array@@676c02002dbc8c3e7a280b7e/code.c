ae15cbbcec3d87c382052fbaa2eb99c4d632ee0f
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-29 10:57:18 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,62 +1,31 @@
 // Your code here...
-// #include<stdio.h>
-// int main(){
-//     int N;
-//     scanf("%d",&N);
-//     int arr[N];
-//     for(int i=0;i<N;i++){
-//         scanf("%d ",&arr[i]);
-//     }
-//     int j=N-1;
-    
-//      for(int i=0;i<N;i++){
-//         if(arr[i]==0){
-//            int temp=arr[i];
-//            arr[i]=arr[j];
-//            arr[j]=arr[i];
-//            j--;
+#include<stdio.h>
+void swapzero(int arr[],int N){
+    int i=0;
+    int j=n-1;
+      for(int i=0;i<N;i++){
+        if(arr[i]==0){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j--;
 
-
-//         }
-//     }
-//      for(int i=0;i<N;i++){
-//         printf("%d ",arr[i]);
-//     }
-// }
-
-
-
-#include <stdio.h>
-
-int main() {
-    int N;
-    scanf("%d", &N);
-
-    int arr[N];
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]); // Removed extra space
-    }
-
-    int j = N - 1; // Last position
-
-    for (int i = 0; i < j; i++) {
-        if (arr[i] == 0) {
-            // Swap zero with last non-zero element
-            while (arr[j] == 0 && j > i) { // Ensure j doesn't go past i
-                j--;
-            }
-            if (j > i) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j--; // Move index leftward
-            }
         }
+        return;
     }
-
-    for (int i = 0; i < N; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
 }
+int main(){
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for(int i=0;i<N;i++){
+        scanf("%d ",&arr[i]);
+    }
+    
+     for(int i=0;i<N;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+
+
