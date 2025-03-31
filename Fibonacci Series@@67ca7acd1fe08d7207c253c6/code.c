36acc8f98e4afb9f04b17e503c626246e4711e9d
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-31 19:01:48 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,13 +1,41 @@
-// Your code here...
-#include <stdio.h>
-int fibonacciSeries(int n){
-    if(n==0 || n==1){
-        if(n==0) return 0;
-        else return 1;
-    }
-    return fibonacciSeries(n-1)+fibonacciSeries(n-2);
-}
+// // Your code here...
+// #include <stdio.h>
+// int fibonacciSeries(int n){
+//     if(n==0 || n==1){
+//         if(n==0) return 0;
+//         else return 1;
+//     }
+//     return fibonacciSeries(n-1)+fibonacciSeries(n-2);
+// }
 
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+    
+//     if (n <= 0) {
+//         printf("Invalid input\n");
+//         return 0;
+//     }
+//     for(int i=0;i<n;i++){
+//         printf("%d ", fibonacciSeries(i));
+//     }
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+void fibonacciSeries(int n) {
+    int a = 0, b = 1, temp;
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    printf("\n"); // Formatting improvement
+}
 
 int main() {
     int n;
@@ -17,8 +45,9 @@ int main() {
         printf("Invalid input\n");
         return 0;
     }
-    for(int i=0;i<n;i++){
-        printf("%d ", fibonacciSeries(i));
-    }
+
+    printf("Fibonacci Series: ");
+    fibonacciSeries(n);
+
     return 0;
 }
