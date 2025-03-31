7851077b052b11aaa9c0585abfd1cd7e1e0bfa59
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-31 15:25:38 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -3,9 +3,11 @@
 void reverse(int arr[],int N){
     int i=0;
     int j=N-1;
+    while(i<j){
     int temp=arr[i];
     arr[i]=arr[j];
     arr[j]=temp;
+    }
     if(arr[N]=reverse(arr,N)){
         printf("YES");
     }
