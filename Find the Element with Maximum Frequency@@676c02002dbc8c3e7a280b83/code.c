77ff68e5c03660f93004447db0e7ba71ae6ea966
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-04-01 01:25:47 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -1,62 +1,24 @@
 // Your code here...
-// #include<stdio.h>
-// #include<limits.h>
-// int main(){
-//     int max=INT_MIN;
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for (int i = 0; i < n; i++) {
-//         scanf("%d ", &arr[i]);
-//     }
-//     for(int i=0;i<n;i++){
-//        if(max<arr[i]){
-//         max=arr[i];
-//        }
-//     }   
-//     for(int i=0;i<n-1;i++){
-//         if(arr[i]==arr[i+1] && arr[i] ==max){
-//             printf("%d",arr[i]);
-//         }
-//     }
-// }
-
-
-#include <stdio.h>
-#include <limits.h>
-
-int main() {
-    int max = INT_MIN;
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int max=INT_MIN;
     int n;
-    scanf("%d", &n);
-    
+    scanf("%d",&n);
     int arr[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d ", &arr[i]);
     }
-
-    // Finding the maximum element
-    for (int i = 0; i < n; i++) {
-        if (max < arr[i]) {
-            max = arr[i];
+    for(int i=0;i<n;i++){
+       if(max<arr[i]){
+        max=arr[i];
+       }
+    }   
+    for(int i=0;i<n-1;i++){
+        if(arr[i]==arr[i+1] && arr[i] ==max){
+            printf("%d",arr[i]);
         }
     }
-
-    // Counting occurrences of max
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == max) {
-            count++;
-        }
-    }
-
-    // Printing max if it appears more than once
-    if (count > 1) {
-        printf("%d", max);
-    }
-
-    return 0;
 }
-
 
 
