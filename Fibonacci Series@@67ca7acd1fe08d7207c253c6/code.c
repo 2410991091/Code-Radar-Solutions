d123e5328d998aa792f697c191d6bfc54744fc60
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-03-31 18:56:33 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,15 +1,16 @@
 // Your code here...
 #include <stdio.h>
-
-int fibonacciSeries(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+int fibonacciSeries(int n){
+    if(n==0 || n==1){
+        if(n==0) return 0;
+        else return 1;
+    }
+    return fibonacciSeries(n-1)+fibonacciSeries(n-2);
 }
+
 
 int main() {
     int n;
-    printf("Enter the number of terms in the Fibonacci series: ");
     scanf("%d", &n);
     
     if (n <= 0) {
@@ -17,11 +18,7 @@ int main() {
         return 0;
     }
 
-    printf("Fibonacci series up to %d terms: ", n);
-    for (int i = 0; i < n; i++) {
-        printf("%d ", fibonacciSeries(i));
-    }
-    printf("\n");
+   fibonacciSeries(n);
 
     return 0;
 }
