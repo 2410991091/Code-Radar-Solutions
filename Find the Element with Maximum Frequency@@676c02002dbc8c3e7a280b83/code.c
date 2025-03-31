9fc0a4cbe817ee97d80629fa8c50a3cf72e8c84e
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2025-04-01 01:08:00 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -1,22 +1,58 @@
 // Your code here...
-#include<stdio.h>
-#include<limits.h>
-int main(){
-    int max=INT_MIN;
+// #include<stdio.h>
+// #include<limits.h>
+// int main(){
+//     int max=INT_MIN;
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d ", &arr[i]);
+//     }
+//     for(int i=0;i<n;i++){
+//        if(max<arr[i]){
+//         max=arr[i];
+//        }
+//     }   
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i]==arr[i+1] && arr[i] ==max){
+//             printf("%d",arr[i]);
+//         }
+//     }
+// }
+
+
+
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int max = INT_MIN;
     int n;
-    scanf("%d",&n);
+
+    // Taking input for array size
+    scanf("%d", &n);
+
     int arr[n];
+
+    // Taking input for array elements
     for (int i = 0; i < n; i++) {
-        scanf("%d ", &arr[i]);
+        scanf("%d", &arr[i]); // Removed extra space
     }
-    for(int i=0;i<n;i++){
-       if(max<arr[i]){
-        max=arr[i];
-       }
-    }   
-    for(int i=0;i<n-1;i++){
-        if(arr[i]==arr[i+1] && arr[i] ==max){
-            printf("%d",arr[i]);
+
+    // Finding the maximum element
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
     }
+
+    // Checking for consecutive max values
+    for (int i = 0; i < n - 1; i++) {  // Avoid out-of-bounds error
+        if (arr[i] == arr[i + 1] && arr[i] == max) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
 }
